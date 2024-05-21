99322a575423fea333e60996317fcee93623cbdf
samm002 <samuelsdjodis@gmail.com>
2024-05-21 09:07:15 +0000

    (feat)adding pm2 config, changing port

--- app/models/lampState.js
+++ app/models/lampState.js
@@ -1,6 +1,14 @@
 const dataFormat = (data) => {
   const getTime = new Date(data._time)
-  const locatTime = getTime.toLocaleString()
+  const options = {
+    timeZone: 'Asia/Shanghai', // Set timezone to Jakarta (GMT+7)
+    hour12: false, // Set to true if you want 12-hour format
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  };
+  const locatTime = getTime.toLocaleString('en-US', options);
+  // const locatTime = getTime.toLocaleString()
   return {
     device_id: data.device_id,
     state: data.state ? "on" : "off",
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const { server } = require('./app')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
